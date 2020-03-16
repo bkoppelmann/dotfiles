5aeea32d18df2915c0953245d2eef45b710da39b
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2020-03-16 17:02:30 +0100

    Add install script
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+make_sym() {
+    if [[ -f $2 || -d $2 ]]; then
+        echo "'$2' already exists, delete? ([y]es/[n]o/[e]xit)"
+        read delete
+        if [ $delete == 'y' ]
+        then
+            if [[ -L $2 || -f $2 ]]
+            then
+                rm $2
+            else
+                rm -r $2
+            fi
+        elif [ $delete == 'n' ]
+        then
+            echo "Skipping '$2'..."
+            return
+        else
+            exit 1
+        fi
+    fi
+
+    ln -s $1 $2
+}
+
+dir=`dirname $0`
+dothome=`realpath $dir`
+home=`realpath ~`
+
+make_sym "$dothome/bashrc" "$home/.bashrc"
+make_sym "$dothome/nvim" "$home/.config/nvim"
+make_sym "$dothome/ctags" "$home/.ctags"
+make_sym "$dothome/ncmpcpp" "$home/.ncmpcpp"
+make_sym "$dothome/spacemacs" "$home/.spacemacs"
+make_sym "$dothome/neomutt" "$home/.config/neomutt"
