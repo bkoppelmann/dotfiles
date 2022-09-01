b3665717e2215ebfbf341803d49f245774672e6d
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2022-09-01 13:37:32 +0200

    nvim/config: Set indent to 4
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -6,6 +6,7 @@ o.swapfile = false
 o.hlsearch = true
 o.ignorecase = true
 o.tabstop = 4
+o.shiftwidth=4
 o.cursorline = true
 o.lazyredraw = true
 vim.cmd("hi CursorLine cterm=NONE ctermbg=Black guibg=Black guifg=NONE")
