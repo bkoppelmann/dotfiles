fdf110d142a9e222175d354e233bac0d1898482d
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2022-10-21 14:29:43 +0200

    nvim: Treesitter remove telescope keybindings

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -19,10 +19,3 @@ require'nvim-treesitter.configs'.setup {
     },
 }
 
--- keybindings
-local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pp', telescope.git_files, options)
-vim.keymap.set('n', '<leader>pf', telescope.find_files, options)
-vim.keymap.set('n', '<leader>/', telescope.live_grep, options)
-vim.keymap.set('n', '<leader>*', telescope.grep_string, options)
-
