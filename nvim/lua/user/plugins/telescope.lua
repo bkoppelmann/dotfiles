10af9d0e4d9ccbfe56e9ad9e991036eb785902aa
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2022-10-21 14:30:09 +0200

    nvim/telescope: exit fuzzy search on <ESC>
    I don't want normal mode.

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -1,6 +1,18 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+telescope.setup({
+    defaults = {
+        mappings = {
+            i = {
+                ['<esc>'] = actions.close,
+            }
+        }
+    }
+})
+
 -- keybindings
-local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pp', telescope.git_files, options)
-vim.keymap.set('n', '<leader>pf', telescope.find_files, options)
-vim.keymap.set('n', '<leader>/', telescope.live_grep, options)
-vim.keymap.set('n', '<leader>*', telescope.grep_string, options)
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pp', telescope_builtin.git_files, options)
+vim.keymap.set('n', '<leader>pf', telescope_builtin.find_files, options)
+vim.keymap.set('n', '<leader>/', telescope_builtin.live_grep, options)
+vim.keymap.set('n', '<leader>*', telescope_builtin.grep_string, options)
