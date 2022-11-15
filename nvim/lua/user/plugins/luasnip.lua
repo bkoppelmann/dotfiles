7f1caf5f6a744db51ea9d8e60c480c33ed72e119
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2022-11-15 09:50:30 +0100

    nvim/luasnip: Fix spelling of mfg
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- nvim/lua/user/plugins/luasnip.lua
+++ nvim/lua/user/plugins/luasnip.lua
@@ -1,7 +1,7 @@
 local snip = require('luasnip')
 
 snip.add_snippets('mail', {
-    snip.parser.parse_snippet('mfg', 'Mit freundliche Grűßen,\nBastian Koppelmann'),
+    snip.parser.parse_snippet('mfg', 'Mit freundlichen Grüßen,\nBastian Koppelmann'),
     snip.parser.parse_snippet('gr', 'Schönen Gruß,\nBastian'),
     snip.parser.parse_snippet('ch', 'Cheers,\nBastian'),
     snip.parser.parse_snippet('br', 'Best Regards,\nBastian'),
