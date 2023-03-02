b921d9b02993d4fb50a9d3f23a3263d6f040f79f
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2023-03-02 13:32:10 +0100

    nvim/keybinding: Add <C-j/k> to move lines around
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- nvim/lua/user/keybindings.lua
+++ nvim/lua/user/keybindings.lua
@@ -11,6 +11,10 @@ map('n', '<leader>h', ':noh<CR>', options)
 -- Disable annoying command line typo.
 map('n', 'q:', ':q', options)
 
+-- Move lines around
+map('n', '<C-j>', ':move .+1<CR>', options)
+map('n', '<C-k>', ':move .-2<CR>', options)
+
 -- remove trailing ws
 map('n', '<leader>w', "<cmd>lua trim_trailing_whitespaces()<CR>", options)
 
