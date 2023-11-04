536825791a47bd91a0076b3859b4f3ab1193a804
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2023-11-04 08:49:13 +0100

    nvim/neogit: Remove 'BranchPopup' command
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- nvim/lua/user/plugins/neogit.lua
+++ nvim/lua/user/plugins/neogit.lua
@@ -72,8 +72,6 @@ neogit.setup {
   mappings = {
     -- modify status buffer mappings
     status = {
-      -- Adds a mapping with "B" as key that does the "BranchPopup" command
-      ["B"] = "BranchPopup",
     }
   }
 }
